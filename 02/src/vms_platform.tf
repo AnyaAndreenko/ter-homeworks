ed5a2e1b460e19c1ab255596e26f36f18165841c
AnnaAndreenko <anya.andreenko@mail.ru>
2025-03-02 20:04:29 +0300

    Create vms

---
+++ 02/src/vms_platform.tf
@@ -0,0 +1,81 @@
+variable "vm_web_name" {
+  type        = string
+  description = "Имя виртуальной машины"
+  default     = "netology-develop-platform-web"
+}
+
+variable "vm_web_user" {
+  type        = string
+  default     = "ubuntu"
+  description = "user"
+}
+
+variable "vm_web_image" {
+  type        = string
+  default     = "ubuntu-2204-lts"
+  description = "image"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  description = "Объем памяти (в ГБ) для виртуальной машины"
+  default     = 1
+}
+
+variable "vm_web_cores" {
+  type        = number
+  description = "Количество ядер для виртуальной машины"
+  default     = 2
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  description = "Доля ядра для виртуальной машины"
+  default     = 5
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  description = "ID платформы для виртуальной машины"
+  default     = "standard-v1"
+}
+
+
+#теперь для бд
+variable "vm_db_name" {
+  type        = string
+  description = "Имя виртуальной машины"
+  default     = "netology-develop-platform-db"
+}
+
+variable "vm_db_memory" {
+  type        = number
+  description = "Объем памяти (в ГБ) для виртуальной машины"
+  default     = 2
+}
+
+variable "vm_db_cores" {
+  type        = number
+  description = "Количество ядер для виртуальной машины"
+  default     = 2
+}
+
+variable "vm_db_core_fraction" {
+  type        = number
+  description = "Доля ядра для виртуальной машины"
+  default     = 20
+}
+
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  }))
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  description = "ID платформы для виртуальной машины"
+  default     = "standard-v1"
+}
