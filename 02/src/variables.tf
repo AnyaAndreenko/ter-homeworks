95e9dd3e9ed95e5f2091befd317f6dffbdd06cec
AnnaAndreenko <anya.andreenko@mail.ru>
2025-03-02 20:23:38 +0300

    fix file

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -2,12 +2,12 @@
 
 
 variable "cloud_id" {
-  type        = string
+  type        = b1gicro1ra02h5iaig5l
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
-  type        = string
+  type        = b1gasmmc1h1g6r1rgd47
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -16,9 +16,22 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+variable "b_zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "db_cidr" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -27,12 +40,17 @@ variable "vpc_name" {
   default     = "develop"
   description = "VPC network & subnet name"
 }
+variable "vpc_db" {
+  type        = string
+  default     = "db"
+  description = "VPC network & subnet db"
+}
 
 
 ###ssh vars
 
-variable "vms_ssh_root_key" {
-  type        = string
-  default     = "<your_ssh_ed25519_key>"
+/*variable "vms_ssh_root_key" {
+  type        = string 
+  default     = "<your_ssh_ed25519_key>"не хранить тут
   description = "ssh-keygen -t ed25519"
-}
+}*/
