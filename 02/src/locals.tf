423f936024aa447b91b09e1dff920ab089666d52
AnnaAndreenko <anya.andreenko@mail.ru>
2025-03-14 21:19:38 +0300

    fix

--- 02/src/locals.tf
+++ 02/src/locals.tf
@@ -1,10 +1,9 @@
-locals {
+/*locals {
   vm_web_name_full = "${var.vm_web_name}"
   vm_db_name_full  = "${var.vm_db_name}"
 }
-/*
+*/
 locals {
   vm_web_name_full = "${var.vpc_name}-${var.vm_web_name}"
   vm_db_name_full  = "${var.vpc_db}-${var.vm_db_name}"
 }
-*/
