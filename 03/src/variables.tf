6220b430ad092a41016ad33a7a096dc56a55d7ad
AnnaAndreenko <anya.andreenko@mail.ru>
2025-03-16 20:23:33 +0300

    new

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -36,5 +36,9 @@ variable "ssh_key" {
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu" 
+  family = "ubuntu-2204-lts" 
+}
+
+output "ubuntu_id" {
+  value = data.yandex_compute_image.ubuntu.id
 }
